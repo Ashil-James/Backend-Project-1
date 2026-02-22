59b587edf7b0b27e7fe3e85f78dcd86806523b43
Ashil-James <ashilgjames@gmail.com>
2026-02-22 09:56:13 +0530

    feat: learned basic controllers and routing

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-  path: "./env",
+  path: "./.env",
 });
+
+import { app } from "./app.js";
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
